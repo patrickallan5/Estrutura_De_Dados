baf75d5593e641d00855f8485863d2da73d27e37
PatrickAllanDeAbreu <patrickallan5@gmail.com>
2020-10-14 21:51:32 -0300

    exercicio estrutura avancada (pillhas)

---
+++ exerc_estrutura de dados avançada_pilhas.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+// criando as variaveis e vetores(pilha)
+int n = NULL;
+int num=0;
+int pilha[5];
+int i = 0;
+// criando metodo insercao
+int insercao (){
+	if(pilha[i] != NULL){
+		cout << "PILHA CHEIA" << endl;
+	}else{
+		pilha[i] =num;
+	    i++;
+	    for ( int f = i - 1; f >= 0; f--){
+			cout << pilha[f] << "," << endl;
+		}
+  	  
+	};
+}
+// criando metoso 
+int retirada (){
+	if(pilha[0] == NULL){
+		cout << "PILHA VAZIA" << endl;
+	}else{
+		pilha[i-1]= NULL;
+		i--;
+		for ( int f = i - 1; f >= 0; f--){
+			cout << pilha[f] << "," << endl;
+		}
+	};
+}
+
+
+int main(int argc, char** argv)
+{
+	setlocale(0, "");
+	if(pilha[0] == NULL){
+		cout << "PILHA VAZIA" << endl;}
+	// empilhando (10)
+	num = 10;
+	cout << "empilando 10 " << endl;
+	cout << endl;
+	insercao();
+	cout << endl;
+	
+	// empilhando (5)
+	num = 5;
+	cout << "empilando 5 " << endl;
+	cout << endl;
+	insercao();
+	cout << endl;
+	
+	//empilhando (15)	
+	num = 15;
+	cout << "empilando 15 " << endl;
+	cout << endl;
+	insercao();
+	cout << endl;
+	
+	
+	//desimpilhando (15)
+	cout << "desemplilando" << endl;
+	cout << endl;
+	retirada();
+	cout << endl;
+	
+	//empilhando (25)	
+	num = 25;
+	cout << "empilando 25 " << endl;
+	cout << endl;
+	insercao();
+	cout << endl;
+
+	return 0;
+}
